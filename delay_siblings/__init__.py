6165f1130d0dd5985e123a8c0e94456821ac2b60
oakkitten <oakkitten@users.noreply.github.com>
2020-04-13 05:13:04 +0100

    anki 2.1.22 compatibility

--- delay_siblings/__init__.py
+++ delay_siblings/__init__.py
@@ -1,7 +1,7 @@
 ﻿# -*- coding: utf-8 -*-
 
 
-# Copyright (c) 2018 oakkitten
+# Copyright (c) 2018-2020 oakkitten
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -27,27 +27,33 @@
 
 # https://github.com/ankidroid/Anki-Android/wiki/Database-Structure
 
-# noinspection PyUnresolvedReferences
-from aqt import mw
+# to get some type checking, see https://github.com/ankitects/anki-typecheck
+from typing import List, Dict
+from contextlib import suppress
+import random
+
+from anki.cards import Card
+from anki.hooks import addHook
+from anki.utils import stripHTML
+
 # noinspection PyUnresolvedReferences
 from aqt.qt import QAction
-# noinspection PyUnresolvedReferences
+from aqt import mw
 from aqt.utils import tooltip
-# noinspection PyUnresolvedReferences
-from anki.hooks import addHook
-# noinspection PyUnresolvedReferences
-from anki.utils import intTime
-import random
 
 ENABLED = 'sibling_delaying_enabled'
 QUIET = 'sibling_delaying_quiet'
 
+# see the sources of both anki/sched.py and anki/schedv2.py:
+# card types: 0=new, 1=lrn, 2=rev, 3=relrn
+# queue types: 0=new/cram, 1=lrn, 2=rev, 3=day lrn, -1=suspended, -2=buried
+CARD_TYPE_REVIEWING = 2
+QUEUE_TYPE_SUSPENDED = -1
 
-# this method uses some protected members, one to display question without css, and one to modify current queue
-# _getQA(): https://github.com/dae/anki/blob/554ff3d8d2ddd8f3e3f84b63b342cfac731712e5/anki/cards.py#L128
-# _getRevCard: https://github.com/dae/anki/blob/554ff3d8d2ddd8f3e3f84b63b342cfac731712e5/anki/sched.py#L786-L789
+# current deck, only used for storing settings
+deck: Dict
 
-# noinspection PyProtectedMember,PyPep8Naming
+# noinspection PyPep8Naming
 def showAnswer():
     if not deck[ENABLED]:
         return
@@ -55,12 +61,10 @@ def showAnswer():
     siblings = get_siblings(mw.reviewer.card)
     cards_per_note = len(siblings) + 1
 
-    out = []
+    out: List[str] = []
     for sibling in siblings:
         # ignore suspended cards and new/learning cards
-        # type: 0=new, 1=learning, 2=due
-        # queue: same as type, but -1=suspended, -2=user buried, -3=sched buried
-        if sibling.type != 2 or sibling.queue == -1:
+        if sibling.type != CARD_TYPE_REVIEWING or sibling.queue == QUEUE_TYPE_SUSPENDED:
             continue
 
         # get due regardless of whether the deck is filtered or not
@@ -69,7 +73,6 @@ def showAnswer():
         due = sibling.odue if filtered else sibling.due
         due -= mw.col.sched.today
         interval = sibling.ivl
-        question = sibling._getQA()['q']
 
         # when the card is going to appear. 0 is today
         when = due if due > 0 else 0
@@ -79,19 +82,20 @@ def showAnswer():
         if min_new_when > when:
             new_when = random.randint(min_new_when, max_new_when)
             reschedule(sibling, new_when, filtered)
-            try: mw.col.sched._revQueue.remove(sibling.id)
-            except ValueError: pass
+
             if (new_when - when) >= 14 or not deck[QUIET]:
-                out.append(u"Sibling: %s (interval: <b>%s</b> days)<br>" % (question, interval) +
-                           u"Rescheduling: <b>%s</b> → <span style='color: crimson'><b>%s</b></span> days after today"
-                           % (due, new_when))
+                question = stripHTML(sibling.q())
+                out.append(f"Sibling: {question} (interval: <b>{interval}</b> days)<br>"
+                           f"Rescheduling: <b>{due}</b> → <span style='color: crimson'><b>{new_when}</b></span> "
+                           f"days after today")
 
     if out:
-        tooltip(u"<span style='color: green'>" + "<hr>".join(out) + "</span>")
+        tooltip("<span style='color: green'>" + "<hr>".join(out) + "</span>")
 
 
+# see https://github.com/ankitects/anki/blob/6ecf2ffa2c46f2501ddbc3fd778035e715399a98/pylib/anki/sched.py#L985-L986
 # noinspection PyShadowingBuiltins
-def get_siblings(card):
+def get_siblings(card: Card) -> List[Card]:
     return [card.col.getCard(id) for id in card.col.db.list("select id from cards where nid=? and id!=?",
                                                             card.nid, card.id)]
 
@@ -122,31 +126,36 @@ def get_siblings(card):
 # 1500 →  57-74; 38-49
 # 3000 → 78-101; 52-67
 # https://www.desmos.com/calculator/fnh882qnd1
-def calc_new_when(interval, cards_per_note):
-    f = (24.0 * interval + 310)**0.4 - 10
+def calc_new_when(interval: int, cards_per_note: int):
+    f = (24.0 * interval + 310) ** 0.4 - 10
     f = f * 2 / cards_per_note 
     return int(round(f)), int(round(f * 1.3))
 
 
-# reschedule a card. like reschedCards, but don't do anything except change the due time
-# mod and usn are used for synchronization, i guess?
-# https://github.com/dae/anki/blob/554ff3d8d2ddd8f3e3f84b63b342cfac731712e5/anki/sched.py#L1348-L1362
-def reschedule(card, due, filtered):
-    mw.col.db.execute("update cards set %s=?, mod=?, usn=? where id=?" % ("odue" if filtered else "due"), 
-                      mw.col.sched.today + due, intTime(), mw.col.usn(), card.id)
+# reschedule a card and remove it from the current queue, if it is there
+# this method relies on a protected member; see
+# https://github.com/ankitects/anki/blob/351d8a309f7d3c0cae7f818313b1a0e7aac4408a/pylib/anki/sched.py#L566-L573
+# noinspection PyProtectedMember
+def reschedule(sibling: Card, due: int, filtered: bool):
+    due += mw.col.sched.today
+    if filtered:
+        sibling.odue = due
+    else:
+        sibling.due = due
+    sibling.flushSched()
 
+    with suppress(AttributeError, ValueError):
+        mw.col.sched._revQueue.remove(sibling.id)
 
 ########################################################################################################################
 
 
-# noinspection PyUnusedLocal
-def flip_enabled(key):
+def flip_enabled(_key):
     enabled = deck[ENABLED] = not deck[ENABLED]
     menu_quiet.setEnabled(enabled)
 
 
-# noinspection PyUnusedLocal
-def flip_quiet(key):
+def flip_quiet(_key):
     deck[QUIET] = not deck[QUIET]
 
 
@@ -156,14 +165,14 @@ menu_enabled = QAction("Enable sibling delaying", mw, checkable=True, enabled=Fa
 menu_enabled.triggered.connect(flip_enabled)
 mw.form.menuTools.addAction(menu_enabled)
 
-menu_quiet = QAction(u"Don’t notify if a card is delayed by less than 2 weeks", mw, checkable=True, enabled=False)
+menu_quiet = QAction("Don’t notify if a card is delayed by less than 2 weeks", mw, checkable=True, enabled=False)
 menu_quiet.triggered.connect(flip_quiet)
 mw.form.menuTools.addAction(menu_quiet)
 
 
-# noinspection PyUnusedLocal,PyGlobalUndefined,PyPep8Naming,PyShadowingBuiltins
-def afterStateChange(prev, next, *args):
-    if next == "overview":
+# noinspection PyPep8Naming
+def afterStateChange(next_state: str, _prev, *_args):
+    if next_state in ["overview", "review"]:
         global deck
         deck = mw.col.decks.current()
         enabled = deck.setdefault(ENABLED, False)
@@ -174,7 +183,9 @@ def afterStateChange(prev, next, *args):
         menu_quiet.setEnabled(enabled)
     else:
         menu_enabled.setEnabled(False)
+        menu_enabled.setChecked(False)
         menu_quiet.setEnabled(False)
+        menu_quiet.setChecked(False)
 
 
 addHook('showAnswer', showAnswer)
