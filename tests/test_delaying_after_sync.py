b1ce7e2ee9a5effc090e8edfb26942187a0dcd05
oakkitten <oakkitten@users.noreply.github.com>
2022-05-07 22:52:01 +0100

    Tests: test delaying after sync with many reviews

--- tests/test_delaying_after_sync.py
+++ tests/test_delaying_after_sync.py
@@ -13,13 +13,31 @@ from tests.tools.collection import (
 
 
 @try_with_all_schedulers
-def test_addon_reschedules_one_card_after_sync(setup):
+def test_addon_reschedules_one_card_after_sync_that_brings_one_new_review(setup):
     setup.delay_siblings.config.data[setup.delay_siblings.OFFER_TO_DELAY_AFTER_SYNC] = True
 
     review_cards_in_0_5_10_days(setup)
     card2_old_due = get_card(setup.card2_id).due
 
     gui_hooks.sync_will_start()
+
+    review_card1_in_20_days(setup)
+
+    with clock_set_forward_by(days=20):
+        gui_hooks.sync_did_finish()
+        card2_new_due = get_card(setup.card2_id).due
+
+        assert card2_old_due != card2_new_due
+
+
+@try_with_all_schedulers
+def test_addon_reschedules_one_card_after_sync_that_brings_many_new_reviews(setup):
+    setup.delay_siblings.config.data[setup.delay_siblings.OFFER_TO_DELAY_AFTER_SYNC] = True
+
+    gui_hooks.sync_will_start()
+
+    review_cards_in_0_5_10_days(setup)
+    card2_old_due = get_card(setup.card2_id).due
 
     review_card1_in_20_days(setup)
 
